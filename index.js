124fb3dedcfbe78e4b5485f517e2f4e9e9aaacbc
Artem <agaleev583@gmail.com>
2024-12-30 16:27:04 +0300

    feat: add css

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const gameBoard = document.querySelector('.board');
 const startBut = document.querySelector('.board__button');
 const input = document.querySelector('.board__input')
 
-startBut.addEventListener('click', () => {
+
+startBut.addEventListener('click', (e) => {
+    e.preventDefault();
     let collums = input.value;
     let count;
+
     if (collums >= 2 && collums <= 6 && collums % 2 === 0) {
         count = collums * collums; 
     } else {
