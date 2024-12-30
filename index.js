d1c619a8c19d5dd13421a9240c5bb38436931b5a
Artem <agaleev583@gmail.com>
2024-12-30 16:18:34 +0300

    feat: add check

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 const gameBoard = document.querySelector('.board');
+const startBut = document.querySelector('.board__button');
+const input = document.querySelector('.board__input')
+
+startBut.addEventListener('click', () => {
+    let collums = input.value;
+    let count;
+    if (collums >= 2 && collums <= 6 && collums % 2 === 0) {
+        count = collums * collums; 
+    } else {
+        input.value = 4;
+    }
+    createBoard();
+});
+
 
 function createBoard() {
     gameBoard.textContent = "";
