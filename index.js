ab2e1a5ed098ab0c7157f8545fa8123a6071bb53
Artem <agaleev583@gmail.com>
2025-01-05 12:07:10 +0300

    feat: creat board

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const input = document.querySelector('.board__input')
 
 startBut.addEventListener('click', (e) => {
     e.preventDefault();
-    let collums = input.value;
+    let colums = input.value;
     let count;
 
-    if (collums >= 2 && collums <= 6 && collums % 2 === 0) {
-        count = collums * collums; 
+    if (colums >= 2 && colums <= 6 && colums % 2 === 0) {
+        count = colums * colums; 
     } else {
         input.value = 4;
     }
@@ -17,6 +17,27 @@ startBut.addEventListener('click', (e) => {
 });
 
 
-function createBoard() {
+function createBoard(colums) {
     gameBoard.textContent = "";
-}
+
+    const templ = document.querySelector('#gameTableTemplate');
+    console.log(templ);
+    
+    //не находит table 
+    const table = templ.querySelector('.table');
+    const rest = templ.querySelector('.table__button');
+    console.log(table);
+
+    table.style = `grid-template-columns: repeat(${colums}, 1fr);
+    grid-template-rows: repeat(${colums}, 1fr)`;
+
+    gameBoard.append(table);
+    
+    rest.addEventListener('click',() => {
+        location.reload();
+    })
+    
+    gameBoard.append(rest);
+};
+
+
