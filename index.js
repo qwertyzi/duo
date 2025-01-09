9922d0f4bed3ed8813f58bc7bdd0f406396eba95
Artem <agaleev583@gmail.com>
2025-01-09 18:07:41 +0300

    fix: id

--- index.js
+++ index.js
@@ -1,49 +1,60 @@
-const gameBoard = document.querySelector('.board');
-const startBut = document.querySelector('.board__button');
-const input = document.querySelector('.board__input')
+const gameBoard = document.querySelector(".board");
+const form = gameBoard.querySelector('.board__form');
+const startButton = form.querySelector('.board__button');
+const input = form.querySelector('.board__input');
 
+startButton.addEventListener("click", (event) => {
+  event.preventDefault()
+  let columns = input.value;
+  let count;
+  if (input.value >= 2 && input.value <= 6 && input.value % 2 == 0) {
+    count = input.value * input.value;
+  } else {
+    alert("Нужно написать четное число в указанном диапазоне.");
+    return;
+  }
 
-startBut.addEventListener('click', (e) => {
-    e.preventDefault();
-    let colums = input.value;
-    let count;
-
-    if (colums >= 2 && colums <= 6 && colums % 2 === 0) {
-        count = colums * colums; 
-    } else {
-        input.value = 4;
-    }
-    createBoard(count, colums);
+  createBoard(count, columns);
 });
 
 
-function createBoard(count, colums) {
-    gameBoard.textContent = "";
-    
-    const templ = document.querySelector('#gameTableTemplate');
-    const templCont = templ.content.cloneNode(true);
-    const table = templCont.querySelector('.table');
-    const rest = templCont.querySelector('.table__button');
-    
-    for (let i = 0; i < count; i++) {
-        table.append(createCard());
-    }
+function createBoard(count, columns) {
 
-    table.style = `grid-template-columns: repeat(${colums}, 1fr);
-    grid-template-rows: repeat(${colums}, 1fr)`;
+  gameBoard.textContent = "";
 
-    gameBoard.append(table);
-    
-    rest.addEventListener('click',() => {
-        location.reload();
-    })
-    
-    gameBoard.append(rest);
+  const template = document.querySelector('#gameTableTemplate').cloneNode(true).content;
+  const gameTable = template.querySelector('.table');
+  const restartBtn = template.querySelector(".table__button");
+
+  for (let i = 0; i < count; i++) {
+    gameTable.append(createCard());
+  }
+
+  gameTable.style = `
+  grid-template-columns: repeat(${columns}, 1fr);
+  grid-template-rows: repeat(${columns}, 1fr);
+  `;
+
+  gameBoard.append(gameTable);
+
+  restartBtn.addEventListener("click", () => {
+    location.reload();
+  });
+
+  gameBoard.append(restartBtn);
+
 };
 
-function createCard() {
-    const cloneCard = document.querySelector("#cardTemplate").cloneNode(true).content;
-    const card = cloneCard.querySelector(".card");
-    return card;
-};
 
+function createCard(flippedIcon) {
+  // Клонирование шаблона
+  const template = document.querySelector('#cardTemplate').cloneNode(true).content;
+  // Поиск нужного элемента
+  const card = template.querySelector('.card');
+  // Добавление иконки, название которой передаем через параметр flippedIcon
+  card.querySelector('#flippedIcon').classList.add(`fa-${flippedIcon}`);
+  // card.addEventListener('click', (event) => gameLogic(event, card));
+
+  // rutern card означает, что получившийся объект "выбрасывается" в то место, где будет вызвана функция createCard
+  return card;
+}
