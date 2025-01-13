b356cdd6d0c92ac249db635827d274e6888117b7
Artem <agaleev583@gmail.com>
2025-01-13 16:36:32 +0300

    error: line:46

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const gameBoard = document.querySelector(".board");
 const form = gameBoard.querySelector('.board__form');
 const startButton = form.querySelector('.board__button');
 const input = form.querySelector('.board__input');
+const couple = {
+  first: null, 
+  firstClickable: true, 
+  second: null, 
+  secondClickable: true
+}
 
 startButton.addEventListener("click", (event) => {
   event.preventDefault()
@@ -21,7 +27,8 @@ function createIconsArray(initialCount){
   const cardsIcons = ["compass", "cloud", "play", "bolt", "stop", "cogs", "atom", "basketball-ball", "arrows", "angle-left", "bars", "file", "filter", "gear",     "folder", "folder-open", "shield", "scissors", "pen-clip"];
   let cards = cardsIcons.slice(0, Math.floor(initialCount / 2));
   const doubleCards = dublicateElements(cards);
-  const shuffleArray = shuffleArray(doubleCards);
+  const shuffledArray = shuffleArray(doubleCards);
+  return shuffledArray;
 }
 
 function dublicateElements(array){
@@ -31,9 +38,57 @@ function dublicateElements(array){
   });
 
   return newArrCards;
-
-
 }
+
+function gameLogic(card) {
+  if (!couple.firstClickable && !couple.secondClickable) return;
+
+  card.classList.add('flip');
+  
+  if (couple.first === null) {
+    // Если нет, то сохраняем на нее ссылку и считаем кликнутой
+    couple.first = card;
+    couple.firstClickable = false;
+  } else if (couple.second === null && couple.first !== card) {
+    // Если да, то проверяем, кликнута ли вторая карточка и не является ли вторая карточка той же самой карточкой, что и первая, и если нет, то сохраняем ссылку на эту карточку и считаем ее кликнутой
+    couple.second = card;
+    couple.secondClickable = false;
+  }
+
+  if (couple.first === null || couple.second === null) return;
+
+   // Сравниваем классы двух карточек и сохраняем логический результат в переменную (это для повышения читабельности)
+  const isEqual = couple.first.firstElementChild.classList[2] === couple.second.firstElementChild.classList[2];
+
+   // Если классы одинаковы
+  if (isEqual) {
+    etTimeout(() => {
+       // То перекрашиваем их в зеленый с задержкой в 1 секунду
+      couple.first.classList.add('successfully');
+      couple.second.classList.add('successfully');
+
+       // Сбрасываем все ссылки и состояния
+      refresh();
+    }, 1000);
+  } else {
+    setTimeout(() => {
+       // Иначе переворачиваем карточки обратно с задержкой в 1 секунду
+      couple.first.classList.remove('flip');
+      couple.second.classList.remove('flip');
+
+       // Сбрасываем все ссылки и состояния
+      refresh();
+    }, 1000);
+  }
+
+   // Функция сброса ссылок и состояний
+  function refresh() {
+    couple.first = null;
+    couple.second = null;
+    couple.firstClickable = true;
+    couple.secondClickable = true;
+  }
+};
 
 function shuffleArray(array) {
   let currentIndex = array.length;
@@ -43,7 +98,7 @@ function shuffleArray(array) {
   currentIndex--;
   
   const temp = array[currentIndex];
-  
+
   array[currentIndex] = array[randomIndex];
   array[randomIndex] = temp;
   };
@@ -59,9 +114,12 @@ function createBoard(count, columns) {
   const gameTable = template.querySelector('.table');
   const restartBtn = template.querySelector(".table__button");
 
-  for (let i = 0; i < count; i++) {
-    gameTable.append(createCard());
-  }
+  const icons = createIconsArray(count);
+
+  icons.forEach(element => {
+    gameTable.append(createCard(element));
+
+  });
 
   gameTable.style = `
   grid-template-columns: repeat(${columns}, 1fr);
@@ -80,15 +138,14 @@ function createBoard(count, columns) {
 
 
 function createCard(flippedIcon) {
-  // Клонирование шаблона
   const template = document.querySelector('#cardTemplate').cloneNode(true).content;
-  // Поиск нужного элемента
-  const card = template.querySelector('.card');
-  // Добавление иконки, название которой передаем через параметр flippedIcon
-  card.querySelector('#flippedIcon').classList.add(`fa-${flippedIcon}`);
-  // card.addEventListener('click', (event) => gameLogic(event, card));
 
-  // rutern card означает, что получившийся объект "выбрасывается" в то место, где будет вызвана функция createCard
+  const card = template.querySelector('.card');
+
+  card.querySelector('#flippedIcon').classList.add(`fa-${flippedIcon}`);
+  card.addEventListener('click', (event) => gameLogic(event, card));
+
   return card;
 }
 
+//ошибка на 46
